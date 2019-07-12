16fe0884a0720a80f3f8c86a3ebce60affddda4e
Elizabeth A. Fujita <syp004@c28-0113-03.ad.mtu.edu>
2019-07-12 14:15:12 -0400

    syp2

---
+++ app.js
@@ -0,0 +1,95 @@
+// console.log("Today is Thursday.")
+// console.log("Monday")
+// console.log("Tuesday")
+// console.log("Wednesday")
+// console.log("Thursday")
+// console.log("Friday")
+
+// console.warn("This is a warning.")
+// console.error("This is an error.")
+// console.table({a:1, b:3})
+// console.table([0,1,2,4])
+
+// var num=0;
+// var number;
+
+// function myfunction(){
+//     console.log("My first javacript function!")
+//     while(num<1000){
+//         myfunction();
+//         number++;
+//     }
+// }
+
+// function f1(){
+//     alert("This is function f1")
+// }
+// function f2(){
+//     console.log("This is function f2")
+// }
+// function f3(){
+//     console.log("This is function f3")
+// }
+// function f4(){
+//     console.log("This is funciton f4")
+// }
+
+// function add(){
+//     var num1 = 5;
+//     var num2 = 6;
+//     var num3;
+//     num3 = num1 + num2;
+//     alert("The answer is:" + num3);
+// }
+
+// function subtract(){
+//     var num1 = 5;
+//     var num2 = 6;
+//     var num3;
+//     num3 = num1 - num2;
+//     alert("The answer is:" + num3);
+// }
+
+// function multiple(){
+//     var num1 = 5;
+//     var num2 = 6;
+//     var num3;
+//     num3 = num1 * num2;
+//     alert("The answer is:" + num3);
+// }
+
+// function divide(){
+//     var num1 = 5;
+//     var num2 = 6;
+//     var num3;
+//     num3 = num1 / num2;
+//     alert("The answer is:" + num3)
+// }
+
+// function stringtest(){
+//     var word1 = "Good"
+//     var word2 = "Morning"
+//     var word3;
+//     word3 = word1 + "\n" + word2;
+//     alert(word3);
+// }
+
+function Eevee(){
+    document.getElementById("bulb").src =
+    "https://assets.pokemon.com/assets/cms2/img/pokedex/detail/133.png"
+}
+
+function Vaporeon(){
+    document.getElementById("bulb").src =
+    "https://assets.pokemon.com/assets/cms2/img/pokedex/detail/134.png"
+}
+
+function Jolteon(){
+    document.getElementById("bulb").src =
+    "https://assets.pokemon.com/assets/cms2/img/pokedex/detail/135.png"
+}
+
+function Flareon(){
+    document.getElementById("bulb").src =
+    "https://assets.pokemon.com/assets/cms2/img/pokedex/detail/136.png"
+}
